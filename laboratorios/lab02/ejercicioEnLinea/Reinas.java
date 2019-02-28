430fbe2ee75ffa6f5c1589eea158b2831a4cdf65
JPabloPena <38082059+JPabloPena@users.noreply.github.com>
2019-02-27 23:12:00 -0500

    Update Reinas.java

--- laboratorios/lab02/ejercicioEnLinea/Reinas.java
+++ laboratorios/lab02/ejercicioEnLinea/Reinas.java
@@ -11,70 +11,53 @@ import java.util.Scanner;
  * @author Juan Sebastian Sanin y Juan Pablo Peña
  */
 public class Reinas {
-  public static void queens(int n, int x, int y) {
-        queensAux(n,0, new int[n],  x ,  y);
+  public static int total = 0;
+    
+    public static void queens(int n, String tab[][]) {
+        queensAux(n,0, new int[n], tab);
     }
   
-    private static void queensAux(int n, int columna, int[] tablero, int x, int y) {
+    private static void queensAux(int n, int columna, int[] tablero, String tab[][]) {
         if (columna == n) {
-            imprimirTablero(tablero,x,y); 
+            total+=1;
+            System.out.println("Soluciones: "+total);
         }else{
             for(int i=0;i<tablero.length;i++){
                
                 tablero[columna] = i;
                 
-                if (esValidoBK(tablero, columna,x,y)==true){
-                    queensAux(n, columna+1, tablero,x,y);
+                if (esValidoBK(tablero, columna, tab)==true){
+                    queensAux(n, columna+1, tablero, tab);
                 }
             }
             
         }
     }
   
-    public static boolean esValidoBK(int[] tablero, int hastaColumnaI,int x, int y) {
+    public static boolean esValidoBK(int[] tablero, int hastaColumnaI, String tab[][]) {
         for (int i = 0; i <= hastaColumnaI; i++){
             for (int j = i+1; j <= hastaColumnaI; j++){
-                if (Math.abs(tablero[i] - tablero[j]) == Math.abs(i-j)|| tablero[i] == tablero[j]||i==y&&j==x ){                 
-                    return false;                    
+                if (Math.abs(tablero[i] - tablero[j]) == Math.abs(i-j)|| tablero[i] == tablero[j]||tab[i][hastaColumnaI].contains("*")){                 
+                        return false;                   
                 }
             }
         }
         return true;   
     }
-    
-    public static void imprimirTablero(int[] tablero,int x, int y) {
-        int n = tablero.length;
-        System.out.print("    ");
-        for (int i = 0; i < n; ++i)
-            System.out.print(i + " ");
-            System.out.println("\n");
-            for (int i = 0; i < n; ++i) {
-                System.out.print(i + "   ");
-                for (int j = 0; j < n; ++j)
-                    
-                    System.out.print((tablero[i] == j ? "Q" : "#") + " ");
-                    System.out.println();
-                
-            }
-            System.out.println();
-    }
    
     public static void main(String[] args) {
-        int m=1;
-        Scanner n= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Escriba el número de reinas: ");
+        int numReinas = sc.nextInt();
+        System.out.println("Escriba el tablero: ");
         
-        while(m!=0){
-        System.out.println("Escriba el numero de reinas");
-        int p= n.nextInt();
-        
-        System.out.println("Escriba las coordenadas de los muros");
-        System.out.print("x: ");
-        int x=n.nextInt();
-         System.out.print("y: ");
-         int y=n.nextInt();
-        queens(p,x,y);
-        System.out.println("Escriba 0 para terminas");
-        m= n.nextInt();
-        }
-    }  
+            String tab[][] = new String[numReinas][numReinas];
+            for (int i = 0; i < tab.length; i++) {
+                String t = sc.next(); 
+                for (int j = 0; j < tab.length; j++) {                  
+                    tab[i][j] = t.substring(j, j+1);
+                }                
+            }       
+        queens(numReinas, tab);
+    }
 }
