26f8d4cbb2b02e77c8afa7a3a05fb425002c4581
JPabloPena <38082059+JPabloPena@users.noreply.github.com>
2019-03-20 22:03:46 -0500

    Add files via upload

---
+++ laboratorios/lab03/ejercicioEnLinea/Prueba.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Peña
+ */
+public class Prueba {
+    
+    public static void user(){   
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Nodos: ");
+        int n = sc.nextInt();
+        while(n<2||n>105){
+            n = sc.nextInt();
+        }
+        
+        DigraphAL grafo = new DigraphAL(n+1);
+        
+        System.out.print("Arcos: ");
+        int m = sc.nextInt();
+        while(m<0||m>105){
+            m = sc.nextInt();
+        }
+        
+        //Se le asigna el peso a dos nodos
+        for(int i=0;i<m;i++){
+            int ai = sc.nextInt();
+            while(1>ai){
+                ai = sc.nextInt();
+            }
+            int bi = sc.nextInt();
+            while(bi>n){
+                bi = sc.nextInt();
+            }
+            int wi = sc.nextInt();
+            while(wi>106){
+                wi = sc.nextInt();
+            }
+            grafo.addArc(ai, bi, wi);
+        }
+        //recorrido(grafo);
+        System.out.println("1 ");
+            System.out.print(" "+costoMinimo(grafo,1,n));      
+    }
+   
+    public static int costoMinimo(Digraph g, int inicio, int fin) {
+        // complete...
+        int n = g.size;
+        int[] minimo = new int[n];
+        for (int i = 1; i < n; ++i)
+            minimo[i] = Integer.MAX_VALUE;
+            minimo[inicio] = 0;
+            dfs(g, inicio, minimo);
+        return minimo[fin] == Integer.MAX_VALUE ? -1 : minimo[fin];
+    }
+
+    private static void dfs(Digraph g, int v, int[] costo) {
+	ArrayList<Integer> succesors = g.getSuccessors(v);
+        ArrayList<Integer> resultado = new ArrayList<>();
+        if (succesors != null) {
+            int peso;
+            for (Integer u : succesors)
+		if ((peso = costo[v] + g.getWeight(v, u)) < costo[u]) {
+                    costo[u] = peso;
+                    System.out.print(v+" -->: "+u+" ");
+                    if(u==5){System.out.println("");}
+                    dfs(g, u, costo);
+		}
+	}
+    }
+    
+    public static void main(String[] args) {
+        user();
+    }
+}
